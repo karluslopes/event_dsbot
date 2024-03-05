f4794cf4165e223f9439d06aa30a3f5a5435efe2
karluslopes <karluslopes.work@gmail.com>
2024-03-05 14:44:53 -0300

    Definição de variáveis do sistema.

--- event_dsbot.py
+++ event_dsbot.py
@@ -11,20 +11,51 @@ async def on_ready():
     print(f'{bot.user} está online!')
 
 
-
 # PARÂMETROS DE CONFIGURAÇÕES
-# 1. Permissões
 
-admRoles = []
-userRoles = []
-participantRoles = []
+# 1. Permissões
+rolesPermissions = {
+    "administrador": [],
+    "usuario": [],
+    "particiante": [],
+    "visitante": [],
+    }
+autoEnableVisitor = False
 eventRole = None
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+# 2. Canais
+eventChannels = {
+    "voice": None,
+    "ping": None,
+    "split": None,
+    "ranking": None,
+    }
+autoCreateTopic = False
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+# 3. Espólios
+adminFee = 10
+autoEnableSplit = True
+
+# 4. Xp/hora
+xpModifier = 100
+xpBaseLevel = 100 # Constante
+autoMessageLevel = ''
+levelRoles = {}
+autoMessageRoles = {}
+
+# Predefinições
+standardEventTypes = ["evento"]
+presets = {
+    "id": [],
+    "tipo": [],
+    "pode participar": [],
+    "canal de voz": [],
+    "permitir visitante": [],
+    "habilitar split": [],
+    "habilitar tópico": [],
+    "confirmação de inscrição": [],
+    "incluir descrição": [],
+}
+
+
 bot.run(os.getenv("BOT_KEY"))
