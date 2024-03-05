ecc89139e16bfdb1990534dedd1f0e1c86302736
karlus <karluslopes.work@gmail.com>
2024-03-05 14:39:06 -0300

    Definição de variáveis do sistema.

--- event_dsbot.py
+++ event_dsbot.py
@@ -10,4 +10,21 @@ bot = discord.Client(intents=intents)
 async def on_ready():
     print(f'{bot.user} está online!')
 
+
+
+# PARÂMETROS DE CONFIGURAÇÕES
+# 1. Permissões
+
+admRoles = []
+userRoles = []
+participantRoles = []
+eventRole = None
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 bot.run(os.getenv("BOT_KEY"))
